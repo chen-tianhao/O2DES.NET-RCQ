c945ada48b02a78cc54955f3eb99c85a2ba4ea89
chen-tianhao <fm0517@hotmail.com>
2024-04-26 14:13:19 +0800

    Align

--- RunnableDemo/Activity/B.cs
+++ RunnableDemo/Activity/B.cs
@@ -4,31 +4,31 @@ using RunnableDemo.Entity;
 namespace RunnableDemo.Activity
 {
     public class B : BaseActivity<Traveller>
-{
-    public B(bool debugMode = false, int seed = 0) : base(nameof(B), debugMode, seed)
     {
-        NeedExtTryStart = true;
-    }
-
-    public override void TryStart(object obj)
-    {
-        Console.WriteLine($"{ClockTime.ToString("yyyy-MM-dd HH:mm:ss")}  {ActivityName}.TryStart({obj})");
-        Bus? bus = obj is Bus ? obj as Bus : null;
-        if (bus == null) { return; }
-
-        List<Traveller> tmpPassengers = new List<Traveller>();
-        foreach (var passenger in PendingList)
+        public B(bool debugMode = false, int seed = 0) : base(nameof(B), debugMode, seed)
         {
-            ReadyToStartList.Add(passenger);
-            tmpPassengers.Add(passenger);
-            bus.Capacity--;
-            if (bus.Capacity == 0) break;
+            NeedExtTryStart = true;
         }
 
-        for (int i = 0; i < tmpPassengers.Count; i++)
+        public override void TryStart(object obj)
         {
-            AttemptToStart();
+            Console.WriteLine($"{ClockTime.ToString("yyyy-MM-dd HH:mm:ss")}  {ActivityName}.TryStart({obj})");
+            Bus? bus = obj is Bus ? obj as Bus : null;
+            if (bus == null) { return; }
+
+            List<Traveller> tmpPassengers = new List<Traveller>();
+            foreach (var passenger in PendingList)
+            {
+                ReadyToStartList.Add(passenger);
+                tmpPassengers.Add(passenger);
+                bus.Capacity--;
+                if (bus.Capacity == 0) break;
+            }
+
+            for (int i = 0; i < tmpPassengers.Count; i++)
+            {
+                AttemptToStart();
+            }
         }
     }
-}
 }
